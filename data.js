0642e9f52d6e9292e3599c4525d41314bfa5dd13
Loris Degioanni <loris@draios.com>
2015-09-14 08:20:33 -0700

    initial commit

---
+++ data.js
@@ -0,0 +1,168 @@
+AvgData = {
+  "":{
+    "t":0,
+    "ch":{
+      "srvc_node1":{
+        "t":1169837.5,
+        "exe":"python /src/us.py",
+        "ch":{
+          "req0":{
+            "t":251396,
+            "exe":"python /src/us.py",
+            "cont":"srvc_node1",
+            "tt":327204.5,
+            "ch":{
+              "srvc_node6":{
+                "t":75808.5,
+                "exe":"python /src/us.py",
+                "cont":"srvc_node6",
+                "tt":75808.5,
+                "ch":[]
+              }
+            }
+          }
+        },
+        "n":2,
+        "tt":1497042,
+        "cont":"srvc_node1"
+      },
+      "srvc_node2":{
+        "t":7802447.5,
+        "exe":"python /src/us.py",
+        "ch":{
+          "req0":{
+            "t":324673.5,
+            "exe":"python /src/us.py",
+            "cont":"srvc_node2",
+            "tt":417494,
+            "ch":{
+              "srvc_node6":{
+                "t":92820.5,
+                "exe":"python /src/us.py",
+                "cont":"srvc_node6",
+                "tt":92820.5,
+                "ch":[]
+              }
+            }
+          }
+        },
+        "n":2,
+        "tt":8219941.5,
+        "cont":"srvc_node2"
+      }
+    },
+    "tt":0
+  }
+};
+MinData = {
+  "":{
+    "t":0,
+    "ch":{
+      "srvc_node1":{
+        "t":818689,
+        "exe":"python /src/us.py",
+        "ch":{
+          "req0":{
+            "t":240633,
+            "exe":"python /src/us.py",
+            "cont":"srvc_node1",
+            "tt":318773,
+            "ch":{
+              "srvc_node6":{
+                "t":78140,
+                "exe":"python /src/us.py",
+                "cont":"srvc_node6",
+                "tt":78140,
+                "ch":[]
+              }
+            }
+          }
+        },
+        "n":1,
+        "tt":1137462,
+        "cont":"srvc_node1"
+      },
+      "srvc_node2":{
+        "t":732248,
+        "exe":"python /src/us.py",
+        "ch":{
+          "req0":{
+            "t":252736,
+            "exe":"python /src/us.py",
+            "cont":"srvc_node2",
+            "tt":334486,
+            "ch":{
+              "srvc_node6":{
+                "t":81750,
+                "exe":"python /src/us.py",
+                "cont":"srvc_node6",
+                "tt":81750,
+                "ch":[]
+              }
+            }
+          }
+        },
+        "n":1,
+        "tt":1066734,
+        "cont":"srvc_node2"
+      }
+    },
+    "tt":0
+  }
+};
+MaxData = {
+  "":{
+    "t":0,
+    "ch":{
+      "srvc_node1":{
+        "t":1520986,
+        "exe":"python /src/us.py",
+        "ch":{
+          "req0":{
+            "t":262159,
+            "exe":"python /src/us.py",
+            "cont":"srvc_node1",
+            "tt":335636,
+            "ch":{
+              "srvc_node6":{
+                "t":73477,
+                "exe":"python /src/us.py",
+                "cont":"srvc_node6",
+                "tt":73477,
+                "ch":[]
+              }
+            }
+          }
+        },
+        "n":1,
+        "tt":1856622,
+        "cont":"srvc_node1"
+      },
+      "srvc_node2":{
+        "t":14872647,
+        "exe":"python /src/us.py",
+        "ch":{
+          "req0":{
+            "t":396611,
+            "exe":"python /src/us.py",
+            "cont":"srvc_node2",
+            "tt":500502,
+            "ch":{
+              "srvc_node6":{
+                "t":103891,
+                "exe":"python /src/us.py",
+                "cont":"srvc_node6",
+                "tt":103891,
+                "ch":[]
+              }
+            }
+          }
+        },
+        "n":1,
+        "tt":15373149,
+        "cont":"srvc_node2"
+      }
+    },
+    "tt":0
+  }
+};
