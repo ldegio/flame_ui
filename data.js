eb76d0da7885aa473ea49279ec8824300663b427
Loris Degioanni <loris@draios.com>
2015-09-17 12:02:00 -0700

    some work on the visualization

--- data.js
+++ data.js
@@ -3,52 +3,77 @@ AvgData = {
     "t":0,
     "ch":{
       "srvc_node1":{
-        "t":1169837.5,
-        "exe":"python /src/us.py",
+        "t":null,
         "ch":{
           "req0":{
-            "t":251396,
-            "exe":"python /src/us.py",
-            "cont":"srvc_node1",
-            "tt":327204.5,
+            "t":null,
+            "tt":null,
             "ch":{
-              "srvc_node6":{
-                "t":75808.5,
-                "exe":"python /src/us.py",
-                "cont":"srvc_node6",
-                "tt":75808.5,
-                "ch":[]
+              "srvc_node2":{
+                "t":null,
+                "ch":{
+                  "req0":{
+                    "t":null,
+                    "exe":"python /src/us.py",
+                    "cont":"srvc_node2",
+                    "tt":null,
+                    "ch":{
+                      "srvc_node4":{
+                        "t":null,
+                        "exe":"python /src/us.py",
+                        "cont":"srvc_node4",
+                        "tt":null,
+                        "ch":{
+                          "processing":{
+                            "t":null,
+                            "exe":"python /src/us.py",
+                            "cont":"srvc_node4",
+                            "tt":null,
+                            "ch":{
+                              "prepare":{
+                                "t":null,
+                                "exe":"python /src/us.py",
+                                "cont":"srvc_node4",
+                                "tt":null,
+                                "ch":[]
+                              },
+                              "run":{
+                                "t":null,
+                                "exe":"python /src/us.py",
+                                "cont":"srvc_node4",
+                                "tt":null,
+                                "ch":[]
+                              },
+                              "reduce":{
+                                "t":null,
+                                "exe":"python /src/us.py",
+                                "cont":"srvc_node4",
+                                "tt":null,
+                                "ch":[]
+                              }
+                            }
+                          },
+                          "data_write":{
+                            "t":null,
+                            "exe":"python /src/us.py",
+                            "cont":"srvc_node4",
+                            "tt":null,
+                            "ch":[]
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+                "tt":null,
+                "nconc":2
               }
             }
           }
         },
-        "n":2,
-        "tt":1497042,
-        "cont":"srvc_node1"
-      },
-      "srvc_node2":{
-        "t":7802447.5,
-        "exe":"python /src/us.py",
-        "ch":{
-          "req0":{
-            "t":324673.5,
-            "exe":"python /src/us.py",
-            "cont":"srvc_node2",
-            "tt":417494,
-            "ch":{
-              "srvc_node6":{
-                "t":92820.5,
-                "exe":"python /src/us.py",
-                "cont":"srvc_node6",
-                "tt":92820.5,
-                "ch":[]
-              }
-            }
-          }
-        },
-        "n":2,
-        "tt":8219941.5,
-        "cont":"srvc_node2"
+        "id":0,
+        "tt":null,
+        "n":0
       }
     },
     "tt":0
@@ -59,108 +84,77 @@ MinData = {
     "t":0,
     "ch":{
       "srvc_node1":{
-        "t":818689,
-        "exe":"python /src/us.py",
+        "t":0,
         "ch":{
           "req0":{
-            "t":240633,
-            "exe":"python /src/us.py",
-            "cont":"srvc_node1",
-            "tt":318773,
+            "t":0,
+            "tt":0,
             "ch":{
-              "srvc_node6":{
-                "t":78140,
-                "exe":"python /src/us.py",
-                "cont":"srvc_node6",
-                "tt":78140,
-                "ch":[]
+              "srvc_node2":{
+                "t":-212501046,
+                "ch":{
+                  "req0":{
+                    "t":1392274,
+                    "exe":"python /src/us.py",
+                    "cont":"srvc_node2",
+                    "tt":212501046,
+                    "ch":{
+                      "srvc_node4":{
+                        "t":201631608,
+                        "exe":"python /src/us.py",
+                        "cont":"srvc_node4",
+                        "tt":211108772,
+                        "ch":{
+                          "processing":{
+                            "t":37274,
+                            "exe":"python /src/us.py",
+                            "cont":"srvc_node4",
+                            "tt":6679061,
+                            "ch":{
+                              "prepare":{
+                                "t":1711695,
+                                "exe":"python /src/us.py",
+                                "cont":"srvc_node4",
+                                "tt":1711695,
+                                "ch":[]
+                              },
+                              "run":{
+                                "t":3507414,
+                                "exe":"python /src/us.py",
+                                "cont":"srvc_node4",
+                                "tt":3507414,
+                                "ch":[]
+                              },
+                              "reduce":{
+                                "t":1422678,
+                                "exe":"python /src/us.py",
+                                "cont":"srvc_node4",
+                                "tt":1422678,
+                                "ch":[]
+                              }
+                            }
+                          },
+                          "data_write":{
+                            "t":2798103,
+                            "exe":"python /src/us.py",
+                            "cont":"srvc_node4",
+                            "tt":2798103,
+                            "ch":[]
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+                "tt":0,
+                "nconc":2
               }
             }
           }
         },
-        "n":1,
-        "tt":1137462,
-        "cont":"srvc_node1"
-      },
-      "srvc_node2":{
-        "t":732248,
-        "exe":"python /src/us.py",
-        "ch":{
-          "req0":{
-            "t":252736,
-            "exe":"python /src/us.py",
-            "cont":"srvc_node2",
-            "tt":334486,
-            "ch":{
-              "srvc_node6":{
-                "t":81750,
-                "exe":"python /src/us.py",
-                "cont":"srvc_node6",
-                "tt":81750,
-                "ch":[]
-              }
-            }
-          }
-        },
-        "n":1,
-        "tt":1066734,
-        "cont":"srvc_node2"
-      }
-    },
-    "tt":0
-  }
-};
-MaxData = {
-  "":{
-    "t":0,
-    "ch":{
-      "srvc_node1":{
-        "t":1520986,
-        "exe":"python /src/us.py",
-        "ch":{
-          "req0":{
-            "t":262159,
-            "exe":"python /src/us.py",
-            "cont":"srvc_node1",
-            "tt":335636,
-            "ch":{
-              "srvc_node6":{
-                "t":73477,
-                "exe":"python /src/us.py",
-                "cont":"srvc_node6",
-                "tt":73477,
-                "ch":[]
-              }
-            }
-          }
-        },
-        "n":1,
-        "tt":1856622,
-        "cont":"srvc_node1"
-      },
-      "srvc_node2":{
-        "t":14872647,
-        "exe":"python /src/us.py",
-        "ch":{
-          "req0":{
-            "t":396611,
-            "exe":"python /src/us.py",
-            "cont":"srvc_node2",
-            "tt":500502,
-            "ch":{
-              "srvc_node6":{
-                "t":103891,
-                "exe":"python /src/us.py",
-                "cont":"srvc_node6",
-                "tt":103891,
-                "ch":[]
-              }
-            }
-          }
-        },
-        "n":1,
-        "tt":15373149,
-        "cont":"srvc_node2"
+        "id":3,
+        "tt":0,
+        "n":0
       }
     },
     "tt":0
